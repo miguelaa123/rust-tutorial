21defc9136de000825d80ed18d6908b450ecbf81
Miguel Alvarado Dorado <miguelaa@unc.edu>
2025-01-27 01:52:45 +0000

    finished tutoral

---
+++ hello-comp423/target/debug/deps/hello_comp423-81d58ecd0f1dd552.d
@@ -0,0 +1,5 @@
+/workspaces/rust-devcontainer/hello-comp423/target/debug/deps/hello_comp423-81d58ecd0f1dd552: src/main.rs
+
+/workspaces/rust-devcontainer/hello-comp423/target/debug/deps/hello_comp423-81d58ecd0f1dd552.d: src/main.rs
+
+src/main.rs:
---
+++ hello-comp423/target/debug/hello-comp423.d
@@ -0,0 +1,1 @@
+/workspaces/rust-devcontainer/hello-comp423/target/debug/hello-comp423: /workspaces/rust-devcontainer/hello-comp423/src/main.rs
